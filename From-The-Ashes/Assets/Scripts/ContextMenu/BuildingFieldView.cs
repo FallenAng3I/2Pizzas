02298f97d24ccc1739a6aecb7353a4ce8165aeca
tutugol <s4mararunner@mail.ru>
2024-04-07 15:40:20 +0300

    try ti Ziz

--- From-The-Ashes/Assets/Scripts/ContextMenu/BuildingFieldView.cs
+++ From-The-Ashes/Assets/Scripts/ContextMenu/BuildingFieldView.cs
@@ -10,16 +10,16 @@ public class BuildingFieldView : MonoBehaviour, ISelectHandler, IDeselectHandler
     public static BuildingFieldView SelectedBuildingField { get; private set; }
     public static UnityEvent<BuildingFieldView> OnBuildingSelected { get; } = new UnityEvent<BuildingFieldView>();
     public static UnityEvent<BuildingFieldView> OnBuildingDeselected { get; } = new UnityEvent<BuildingFieldView>();
-
+    
+    public void OnSelect(BaseEventData eventData)
+    {
+        SelectedBuildingField = this;
+        OnBuildingSelected.Invoke(this);
+    }
+    
     public void OnDeselect(BaseEventData eventData)
     {
         SelectedBuildingField = null;
         OnBuildingSelected.Invoke(this);
-    }
-     
-    public void OnSelect(BaseEventData eventData)
-    {
-        SelectedBuildingField = this;
-        OnBuildingSelected.Invoke(this); 
     }
 }
