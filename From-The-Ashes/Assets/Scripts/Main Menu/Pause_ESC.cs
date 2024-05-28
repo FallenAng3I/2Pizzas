a6a8bc2046127585594129f9e54e13473819f8b2
tutugol <s4mararunner@mail.ru>
2024-05-28 15:08:15 +0300

    Pause menu done

---
+++ From-The-Ashes/Assets/Scripts/Main Menu/Pause_ESC.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pause_ESC : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    [Header("Панели для выключения в паузе:")]
+    [SerializeField] private GameObject ConstructionMenu;  
+    [SerializeField] private GameObject BuildingMenu;      
+    [SerializeField] private GameObject UpgradeMenu;       
+    [SerializeField] private GameObject MilitaryBaseConstructionMenu;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+    
+    void Pause()
+    {
+        ConstructionMenu.SetActive(false);
+        BuildingMenu.SetActive(false);
+        UpgradeMenu.SetActive(false);
+        MilitaryBaseConstructionMenu.SetActive(false);
+        
+        PauseMenu.SetActive(true);
+        
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    
+    
+    public void Quit()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
